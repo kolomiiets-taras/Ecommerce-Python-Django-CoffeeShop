4f9e06012b3794ab5698e667f050cdc2e91268da
Kolomiiets Taras <kolomiiets.taras@gmail.com>
2022-09-09 16:01:10 +0300

    add media to gitignore

--- CoffeeStation/settings.py
+++ CoffeeStation/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'easy_thumbnails',
     'accounts.apps.AccountsConfig',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,12 +111,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'backend/static'),
     )
 
 # Default primary key field type
